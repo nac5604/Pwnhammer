d03a2ec9e292abc87c3be2bcd65bf303a5992621
nac5604 <85867267+nac5604@users.noreply.github.com>
2023-01-10 20:25:54 -0500

    Update KilledProcessesDialog.cs

--- PwnHammer/Forms/KilledProcessesDialog.cs
+++ PwnHammer/Forms/KilledProcessesDialog.cs
@@ -31,14 +31,7 @@ namespace PwnHammer
             Random RandomGen = new Random();
             int randomValue = RandomGen.Next(100);
 
-            if (randomValue < 40)
-            {
-                label1.Text = String.Format("Aw hell naw he done kilt {0} of the got dam processors! 😢", killedProcsAmount);
-            }
-            else
-            {
-                label1.Text = String.Format("PwnHammer killed {0} conflicting processes.", killedProcsAmount);
-            }
+            label1.Text = String.Format("PwnHammer killed {0} conflicting processes.", killedProcsAmount);
 
             this.Icon = PwnHammer.Properties.Resources.Pwnhammer;
 
